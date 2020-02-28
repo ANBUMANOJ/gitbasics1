3592cd079f1f5cf4279e002521656a06f39cdca0
MYPC <sowmi0702@gmail.com>
2020-02-28 12:13:10 +0530

    lululemon added

---
+++ Lululemon/src/test/java/com/lululemon/reports/report.js
@@ -0,0 +1,271 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/Ammu/eclipse-workspace/Lululemon/src/test/resource/com/lululemon/feature/lululemon.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Lulelemon website testing functionality",
+  "description": "",
+  "id": "lulelemon-website-testing-functionality",
+  "keyword": "Feature"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "comments": [
+    {
+      "line": 4,
+      "value": "#Given User initialize the browser"
+    }
+  ],
+  "line": 5,
+  "name": "User enter the lululemon url",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.user_enter_the_lululemon_url()"
+});
+formatter.result({
+  "duration": 18874725900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "Create Account functionality",
+  "description": "",
+  "id": "lulelemon-website-testing-functionality;create-account-functionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 9,
+  "name": "User click the sign in button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "User create an account with email id",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "User create a password for the account",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "User click the create an account button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "User check whether the email already in use header is displayed",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_click_the_sign_in_button()"
+});
+formatter.result({
+  "duration": 10279867700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_create_an_account_with_email_id()"
+});
+formatter.result({
+  "duration": 1197241500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_create_a_password_for_the_account()"
+});
+formatter.result({
+  "duration": 237818300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_click_the_create_an_account_button()"
+});
+formatter.result({
+  "duration": 4708370100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_check_whether_the_email_already_in_use_header_is_displayed()"
+});
+formatter.result({
+  "duration": 396535900,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "comments": [
+    {
+      "line": 4,
+      "value": "#Given User initialize the browser"
+    }
+  ],
+  "line": 5,
+  "name": "User enter the lululemon url",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.user_enter_the_lululemon_url()"
+});
+formatter.result({
+  "duration": 3646603000,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 15,
+  "name": "Signin functionality",
+  "description": "",
+  "id": "lulelemon-website-testing-functionality;signin-functionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 16,
+  "name": "User click the sign in button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 17,
+  "name": "User enter the email id",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "User enter the password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "User click the signin button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 20,
+  "name": "User logout from the application",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_click_the_sign_in_button()"
+});
+formatter.result({
+  "duration": 5524164100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_enter_the_email_id()"
+});
+formatter.result({
+  "duration": 807740600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_enter_the_password()"
+});
+formatter.result({
+  "duration": 312146200,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_click_the_signin_button()"
+});
+formatter.result({
+  "duration": 5770489300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_logout_from_the_application()"
+});
+formatter.result({
+  "duration": 5072795800,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "comments": [
+    {
+      "line": 4,
+      "value": "#Given User initialize the browser"
+    }
+  ],
+  "line": 5,
+  "name": "User enter the lululemon url",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "StepDefinition.user_enter_the_lululemon_url()"
+});
+formatter.result({
+  "duration": 4180797100,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 22,
+  "name": "Forget Password functionality",
+  "description": "",
+  "id": "lulelemon-website-testing-functionality;forget-password-functionality",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 23,
+  "name": "User click the sign in button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 24,
+  "name": "User click the forget password link",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "User the email id for reseting the password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "User click the reset button",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinition.user_click_the_sign_in_button()"
+});
+formatter.result({
+  "duration": 5229450600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_click_the_forget_password_link()"
+});
+formatter.result({
+  "duration": 4242457500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_the_email_id_for_reseting_the_password()"
+});
+formatter.result({
+  "duration": 605841700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinition.user_click_the_reset_button()"
+});
